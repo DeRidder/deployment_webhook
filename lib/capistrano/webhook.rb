59ec9497d88ec6a66419d95269059f23eb769b9d
DeRidder <p.b.deridder@hotmail.com>
2018-11-23 14:22:46 +0100

    load files correctly, update version

--- lib/capistrano/webhook.rb
+++
@@ -1,1 +0,0 @@
-load File.expand_path('../tasks/notify.rake', __FILE__)
--- lib/deployment_webhook.rb
+++ lib/deployment_webhook.rb
@@ -1,6 +1,11 @@
-load File.expand_path('../deployment_webhook/notifier.rb', __FILE__)
-
 module DeploymentWebhook
+  GEM_ROOT = File.dirname(File.dirname(__FILE__))
+  LIB_BASE = File.join(GEM_ROOT, 'lib/deployment_webhook')
+  TASKS_BASE = File.join(GEM_ROOT, 'lib/capistrano/tasks')
+
+  Dir[File.join(LIB_BASE, '*.rb')].each { |file| require(file) }
+  Dir[File.join(TASKS_BASE, '*.rake')].each { |file| load(file) }
+
   def self.send_notification(deployment_webhook_url)
     Notifier.new(deployment_webhook_url).send_message
   end

0dcecbd513495d87181a9e4def6a254b5b0dcf12
DeRidder <p.b.deridder@hotmail.com>
2018-11-23 15:19:27 +0100

    load rake task

--- lib/deployment_webhook.rb
+++ lib/deployment_webhook.rb
@@ -1,8 +1,8 @@
 module DeploymentWebhook
   GEM_ROOT = File.dirname(File.dirname(__FILE__))
-  LIB_BASE = File.join(GEM_ROOT, 'lib', 'deployment_webhook')
-  TASKS_BASE = File.join(GEM_ROOT, 'lib', 'tasks')
+  LIB_BASE = File.join(GEM_ROOT, 'lib/deployment_webhook')
+  TASKS_BASE = File.join(GEM_ROOT, 'lib/tasks')
 
   Dir[File.join(LIB_BASE, '*.rb')].each { |file| require(file) }
-  Dir[File.join(TASKS_BASE, '*.rake')].each { |file| require(file) }
+  load File.join(TASKS_BASE, 'notify.rake')
 end

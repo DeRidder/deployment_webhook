939574b2c3520fe52a542928bbaca2e27543f183
DeRidder <p.b.deridder@hotmail.com>
2018-11-23 14:34:59 +0100

    only load notify rake task

--- lib/deployment_webhook.rb
+++ lib/deployment_webhook.rb
@@ -1,8 +1,8 @@
 module DeploymentWebhook
   GEM_ROOT = File.dirname(File.dirname(__FILE__))
   LIB_BASE = File.join(GEM_ROOT, 'lib/deployment_webhook')
-  TASKS_BASE = File.join(GEM_ROOT, 'lib/capistrano/tasks')
 
   Dir[File.join(LIB_BASE, '*.rb')].each { |file| require(file) }
-  Dir[File.join(TASKS_BASE, '*.rake')].each { |file| load(file) }
+
+  load File.join(GEM_ROOT, 'lib/capistrano/tasks/notify.rake')
 end

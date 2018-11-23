37fd16f3a4e3c289384606db8d38ef1e12723cd5
DeRidder <p.b.deridder@hotmail.com>
2018-11-23 15:20:02 +0100

    update tasks base

--- lib/deployment_webhook.rb
+++ lib/deployment_webhook.rb
@@ -1,7 +1,7 @@
 module DeploymentWebhook
   GEM_ROOT = File.dirname(File.dirname(__FILE__))
   LIB_BASE = File.join(GEM_ROOT, 'lib/deployment_webhook')
-  TASKS_BASE = File.join(GEM_ROOT, 'lib/tasks')
+  TASKS_BASE = File.join(GEM_ROOT, 'lib/capistrano/tasks')
 
   Dir[File.join(LIB_BASE, '*.rb')].each { |file| require(file) }
   load File.join(TASKS_BASE, 'notify.rake')
